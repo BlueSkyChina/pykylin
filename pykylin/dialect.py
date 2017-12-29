9a4af1e877fafc7e8778bac9133af47c8c34cba6
陈明磊 <57022474@qq.com>
2017-12-29 09:49:40 +0800

    解决sqlalchemy新版本适配
    1、解决原connect参数传递的已经是Engine对象，
    2、同时修复has_table 调用get_table_names方法多传递了table_name参数；

--- pykylin/dialect.py
+++ pykylin/dialect.py
@@ -5,8 +5,8 @@ from sqlalchemy.sql.compiler import *
 from sqlalchemy.engine import default
 from .types import KYLIN_TYPE_MAP
 
-class KylinCompiler(SQLCompiler):
 
+class KylinCompiler(SQLCompiler):
     def visit_label(self, label,
                     add_to_result_map=None,
                     within_label_clause=False,
@@ -36,7 +36,7 @@ class KylinCompiler(SQLCompiler):
                 add_to_result_map(
                     labelname,
                     label.name,
-                    (label, labelname, ) + label._alt_names,
+                    (label, labelname,) + label._alt_names,
                     label.type
                 )
 
@@ -123,6 +123,9 @@ class KylinDialect(default.DefaultDialect):
 
     def __init__(self, **kwargs):
         super(KylinDialect, self).__init__(self, **kwargs)
+        self.returns_unicode_strings = True
+        self.default_isolation_level = None
+        self.default_schema_name = None
 
     @classmethod
     def dbapi(cls):
@@ -130,43 +133,52 @@ class KylinDialect(default.DefaultDialect):
 
     def initialize(self, connection):
         self.server_version_info = None
-        self.default_schema_name = None
-        self.default_isolation_level = None
-        self.returns_unicode_strings = True
 
     def create_connect_args(self, url):
         opts = url.translate_connect_args()
+        api_prefix = 'kylin/api'
         args = {
             'username': opts['username'],
             'password': opts['password'],
-            'endpoint': 'http://%s:%s/%s' % (opts['host'], opts['port'], opts['database'])
+            'endpoint': 'http://%s:%s/%s' % (opts['host'], opts['port'], api_prefix)
+            # modified login url
         }
         args.update(url.query)
         return [], args
 
-    def get_table_names(self, connection, schema=None, **kw):
+    def get_schema_names(self, engine, **kw):
+        connection = engine.contextual_connect()
+        return connection.connection.list_schemas()
+
+    def get_table_names(self, engine, schema=None, **kw):
+        connection = engine.contextual_connect()
         return connection.connection.list_tables()
 
-    def has_table(self, connection, table_name, schema=None):
-        return table_name in self.get_table_names(connection, table_name, schema)
+    def has_table(self, engine, table_name, schema=None):
+        return table_name in self.get_table_names(engine, schema)
 
     def has_sequence(self, connection, sequence_name, schema=None):
         return False
 
-    def get_columns(self, connection, table_name, schema=None, **kw):
+    def get_columns(self, engine, table_name, schema=None, **kw):
+        connection = engine.contextual_connect()
         cols = connection.connection.list_columns(table_name)
         return [self._map_column_type(c) for c in cols]
 
     def _map_column_type(self, column):
+        # add type decimal and timestamp
         tpe_NAME = column['type_NAME']
         if tpe_NAME.startswith('VARCHAR'):
             tpe_size = column['column_SIZE']
             args = (tpe_size,)
             tpe = KYLIN_TYPE_MAP['VARCHAR']
-        elif tpe_NAME == 'DECIMAL':
+        elif tpe_NAME.startswith('DECIMAL'):
             digit_size = column['decimal_DIGITS']
             args = (digit_size,)
             tpe = KYLIN_TYPE_MAP['DECIMAL']
+        elif tpe_NAME.startswith('TIMESTAMP'):
+            args = ()
+            tpe = KYLIN_TYPE_MAP['TIMESTAMP']
         else:
             args = ()
             tpe = KYLIN_TYPE_MAP[tpe_NAME]
@@ -191,4 +203,3 @@ class KylinDialect(default.DefaultDialect):
     def get_unique_constraints(
             self, connection, table_name, schema=None, **kw):
         return []
-
