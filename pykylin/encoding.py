de72a8321f4ad652bbd8cd841ffb150f7bf7cf14
陈明磊 <57022474@qq.com>
2018-01-23 08:58:12 +0800

    格式化调整
    1、修复md文件路径读取后缀名；
    2、格式化一些代码

--- pykylin/encoding.py
+++ pykylin/encoding.py
@@ -3,15 +3,16 @@ from __future__ import absolute_import
 from json import JSONEncoder, JSONDecoder, loads
 from json.decoder import WHITESPACE
 
-class KylinJSONEncoder(JSONEncoder):
 
+class KylinJSONEncoder(JSONEncoder):
     def default(self, o):
         return super(KylinJSONEncoder, self).default(self, o)
 
-class KylinJSONDecoder(JSONDecoder):
 
+class KylinJSONDecoder(JSONDecoder):
     def decode(self, s, _w=WHITESPACE.match):
         return super(KylinJSONDecoder, self).decode(s)
+
 
 def decode(s):
     return loads(s, cls=KylinJSONDecoder)
--- pykylin/errors.py
+++ pykylin/errors.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
-class Error(Exception):
 
+class Error(Exception):
     def __init__(self, msg):
         super(Error, self).__init__(msg)
         self.msg = msg
